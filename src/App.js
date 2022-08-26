fb1e201f8ec1125fea1576fce4a907133fa63800
Will Ramirez <wramirez1980@gmail.com>
2022-08-26 00:42:13 -0500

    styling updates

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.scss";
 import Form from "./components/form/Form";
 import Jumbotron from "./components/jumbotron/Jumbotron";
 import Navigation from "./components/navigation/Navigation";
-import icon from "./assets/icon.svg"
+import icon from "./assets/icon.svg";
 import CarouselSlider from "./components/carousel/Carousel";
 function App() {
   return (
@@ -88,27 +88,36 @@ function App() {
             </ul>
           </div>
         </div>
-        
       </div>
       <div className="container-fluid bg-light">
         <CarouselSlider></CarouselSlider>
       </div>
 
-      <div className="container pb-5">
+      <div className="container pb-5 ">
         <div className="row">
-        <div className="col-6 mt-5 mb-5"><img src={icon} width={263} height={263} alt=""/>&nbsp;<span>CENTER POINT</span>
-        <h3 className="mt-5 text-start">Promising subheading</h3>
-        <p className="text-start">At rerum harum cum minima error eos voluptates dignissimos non
-                ipsam voluptas aut asperiores repellat ut nihil amet.</p></div>
-       
-        <div className="col-6 bg-dark mt-5 mb-5"><Form></Form></div>
+          <div className="col-6 mt-5 mb-5 d-flex flex-column justify-content-center">
+            
+            <div><img src={icon} width={263} height={263} alt="" />
+            &nbsp;<span>CENTER POINT</span></div>
+            <h3 className="mt-5 text-start">Promising subheading</h3>
+            <p className="text-start">
+              At rerum harum cum minima error eos voluptates dignissimos non
+              ipsam voluptas aut asperiores repellat ut nihil amet.
+            </p>
+          </div>
+
+          <div className="col-6 bg-dark mt-5 mb-5">
+            <Form></Form>
+          </div>
         </div>
       </div>
       <nav className="navbar  navbar-light bg-light mt-5">
-  <div className="container">
-    <a className="navbar-brand" href="/">Footer</a>
-  </div>
-</nav>
+        <div className="container">
+          <a className="navbar-brand" href="/">
+            Footer
+          </a>
+        </div>
+      </nav>
     </div>
   );
 }
